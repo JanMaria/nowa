9651eba1f6d435372e3d2db633a920c2f361b2b3
JanMaria <jan.maria.prokop@gmail.com>
2018-10-30 22:25:30 +0100

    second video; adding and displaying articles in database

--- YT_CRUD/src/Controller/ArticleController.php
+++ YT_CRUD/src/Controller/ArticleController.php
@@ -1,24 +1,50 @@
 <?php
+
   namespace App\Controller;
+
+  use App\Entity\Article;
 
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\Routing\Annotation\Route;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
   use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-  class ArticleController extends Controller {
+  class ArticleController extends Controller
+  {
     /**
-    * @Route("/")
-    * @Method({"GET"})
-    */
+     * @Route("/", name="article_list")
+     * @Method({"GET"})
+     */
     public function index() {
-      // return new Response('<html><body>Hello</body></html>');
 
-      $articles = ['Article1', 'Article2', 'Article3'];
+      $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
-      return $this->render('articles/index.html.twig', array('articles' => $articles));
+      return $this->render('articles/index.html.twig', ['articles' => $articles]);
     }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+     public function show($id) {
+       $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+       return $this->render('articles/show.html.twig', ['article' => $article]);
+     }
+
+    // /**
+    //  * @Route("/articles/save")
+    //  */
+    //  public function save() {
+    //    $entityManager = $this->getDoctrine()->getManager();
+    //
+    //    $article = new Article();
+    //    $article->setTitle('Article 6');
+    //    $article->setBody('This is the body for article six');
+    //
+    //    $entityManager->persist($article);
+    //
+    //    $entityManager->flush();
+    //
+    //    return new Response('Saved an article with the id of '.$article->getId());
+    //  }
   }
-
-
- ?>
