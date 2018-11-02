4c3023cb5bb8a800fe2f1b0b80b6952e772411d7
JanMaria <jan.maria.prokop@gmail.com>
2018-11-02 19:23:12 +0100

    video tutorial ukończony

--- YT_CRUD/src/Controller/ArticleController.php
+++ YT_CRUD/src/Controller/ArticleController.php
@@ -1,50 +1,157 @@
 <?php
 
-  namespace App\Controller;
+declare(strict_type=1);
 
-  use App\Entity\Article;
+namespace App\Controller;
 
-  use Symfony\Component\HttpFoundation\Response;
-  use Symfony\Component\Routing\Annotation\Route;
-  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-  use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+// use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 
-  class ArticleController extends Controller
+class ArticleController extends AbstractController
+{
+  /**
+  * @Route("/", name="article_list")
+  * @Method({"GET"})
+  */
+  public function index()
   {
-    /**
-     * @Route("/", name="article_list")
-     * @Method({"GET"})
-     */
-    public function index() {
+    $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
-      $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+    return $this->render('articles/index.html.twig', ['articles' => $articles]);
+  }
 
-      return $this->render('articles/index.html.twig', ['articles' => $articles]);
+  /**
+  * @Route("/article/new", name="new_article")
+  * @Method({"GET", "POST"})
+  */
+  public function new(Request $request)
+  {
+    $article = new Article();
+
+    $form = $this->createFormBuilder($article)
+    ->add('title', TextType::class, ['attr' => ['class' => 'form-control']])
+    ->add('body', TextareaType::class, ['required' => false, 'attr' => ['class' => 'form-control']])
+    ->add('save', SubmitType::class, ['label' => 'Create', 'attr' => ['class' => 'btn btn-primary mt-3']])
+    ->getForm();
+
+    $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid()) {
+      $article = $form->getData();
+
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($article);
+      $entityManager->flush();
+
+      return $this->redirectToRoute('article_list');
     }
 
-    /**
-     * @Route("/article/{id}", name="article_show")
-     */
-     public function show($id) {
-       $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
-
-       return $this->render('articles/show.html.twig', ['article' => $article]);
-     }
-
-    // /**
-    //  * @Route("/articles/save")
-    //  */
-    //  public function save() {
-    //    $entityManager = $this->getDoctrine()->getManager();
-    //
-    //    $article = new Article();
-    //    $article->setTitle('Article 6');
-    //    $article->setBody('This is the body for article six');
-    //
-    //    $entityManager->persist($article);
-    //
-    //    $entityManager->flush();
-    //
-    //    return new Response('Saved an article with the id of '.$article->getId());
-    //  }
+    return $this->render('articles/new.html.twig', ['form' => $form->createView()]);
   }
+
+  /**
+  * @Route("/article/edit/{id}", name="edit_article")
+  * @Method({"GET", "POST"})
+  */
+  public function edit(Request $request, Article $article)
+  {
+    // $article = new Article();
+    //$article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+    $form = $this->createFormBuilder($article)
+    ->add('title', TextType::class, ['attr' => ['class' => 'form-control']])
+    ->add('body', TextareaType::class, ['required' => false, 'attr' => ['class' => 'form-control']])
+    ->add('save', SubmitType::class, ['label' => 'Update', 'attr' => ['class' => 'btn btn-primary mt-3']])
+    ->getForm();
+
+    $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid()) {
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->flush();
+
+      return $this->redirectToRoute('article_list');
+    }
+
+    return $this->render('articles/edit.html.twig', ['form' => $form->createView()]);
+  }
+
+  // tutaj stara metoda bez użycia ParamConverter'a
+  // /**
+  // * @Route("/article/{id}", name="article_show")
+  // * @Method({"GET"})
+  // */
+  // public function show($id)
+  // {
+  //   $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+  //
+  //   return $this->render('articles/show.html.twig', ['article' => $article]);
+  // }
+
+  // tutaj, o ile dobrze rozumiem, niejawnie używam ParamConverter'a
+  // /**
+  // * @Route("/article/{id}", name="article_show")
+  // * @Method({"GET"})
+  // */
+  // public function show(Article $article)
+  // {
+  //   return $this->render('articles/show.html.twig', ['article' => $article]);
+  // }
+
+  // a tutaj z jawnym użyciem ParamConverter'a
+  /**
+  * @Route("/article/{articleID}", name="article_show")
+  * @Method({"GET"})
+  * @ParamConverter("article", options={"mapping"={"articleID"="id"}})
+  */
+  public function show(Article $article)
+  {
+    return $this->render('articles/show.html.twig', ['article' => $article]);
+  }
+
+  /**
+   * @Route("/article/delete/{id}")
+   * @Method({"DELETE"})
+   */
+   public function delete(Request $request, Article $article)
+   {
+     // $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+     $entityManager = $this->getDoctrine()->getManager();
+     $entityManager->remove($article);
+     $entityManager->flush();
+
+     $response = new Response();
+     $response->send();
+   }
+
+  // /**
+  //  * @Route("/articles/save")
+  //  */
+  //  public function save() {
+  //    $entityManager = $this->getDoctrine()->getManager();
+  //
+  //    $article = new Article();
+  //
+  //    $article
+  //     ->setTitle('Article 999')
+  //     ->setBody('This is the body for article 999');
+  //    // $article->setTitle('Article 6');
+  //    // $article->setBody('This is the body for article six');
+  //
+  //    $entityManager->persist($article);
+  //
+  //    $entityManager->flush();
+  //
+  //    return new Response('Saved an article with the id of '.$article->getId());
+  //  }
+}
--- YT_CRUD/src/Entity/Article.php
+++ YT_CRUD/src/Entity/Article.php
@@ -1,48 +1,57 @@
 <?php
 
+declare(strict_type=1);
+
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
- */
+* @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+*/
 class Article
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+  /**
+  * @ORM\Id()
+  * @ORM\GeneratedValue()
+  * @ORM\Column(type="integer")
+  */
+  private $id;
 
-    /**
-     * @ORM\Column(type="text", length=100)
-     */
-    private $title;
+  /**
+  * @ORM\Column(type="text", length=100)
+  */
+  private $title;
 
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $body;
+  /**
+  * @ORM\Column(type="text")
+  */
+  private $body;
 
-    public function getId(): ?int {
-        return $this->id;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    public function getTitle() {
-      return $this->title;
-    }
+  public function getTitle(): ?string
+  {
+    return $this->title;
+  }
 
-    public function setTitle($title) {
-      $this->title = $title;
-    }
+  public function setTitle(string $title)
+  {
+    $this->title = $title;
+    return $this;
+  }
 
-    public function getBody() {
-      return $this->body;
-    }
+  public function getBody(): ?string
+  {
+    return $this->body;
+  }
 
-    public function setBody($body) {
-      $this->body = $body;
-    }
+  public function setBody(string $body)
+  {
+    $this->body = $body;
+    return $this;
+  }
 }
